9e060160dbc50e761f8b15d4c6c18f704125c416
Anzel2011 <86427908+Anzel2011@users.noreply.github.com>
2021-12-11 11:24:08 -0600

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCUzXGwB2kWm-HwG1ZnbkR2tZfyz7_IGAE",
+  authDomain: "project-kwitter-7da58.firebaseapp.com",
+  databaseURL: "https://project-kwitter-7da58-default-rtdb.firebaseio.com",
+  projectId: "project-kwitter-7da58",
+  storageBucket: "project-kwitter-7da58.appspot.com",
+  messagingSenderId: "765420908198",
+  appId: "1:765420908198:web:f6e0fe1f3f0020968a8e67"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
